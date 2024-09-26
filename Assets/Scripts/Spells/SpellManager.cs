d8c88de47a9c214af726a21ca7f83d397dbe58a9
Timsel1 <timmeijvogel2003@gmail.com>
2024-09-26 16:59:05 +0200

    🔨 Update Sequence and Spell manager
    Sequence recognition actually works now, i apparently lied before :)

--- Assets/Scripts/Spells/SpellManager.cs
+++ Assets/Scripts/Spells/SpellManager.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace RogueApeStudios.SecretsOfIgnacios.Spells
@@ -6,28 +6,41 @@ namespace RogueApeStudios.SecretsOfIgnacios.Spells
     internal class SpellManager : MonoBehaviour
     {
         [SerializeField] private Spell[] _availableSpells;
-        [SerializeField] private GestureManager _gestureManager;
+        [SerializeField] private SequenceManager _gestureManager;
 
         private Spell _currentSpell;
         private bool _canCast = false;
+
+        private void OnEnable()
+        {
+            _gestureManager.OnSequenceCreated += ValidateSequence;
+        }
+
+        private void OnDestroy()
+        {
+            _gestureManager.OnSequenceCreated -= ValidateSequence;
+        }
+
+        private void SetSpell(Spell spell)
+        {
+            _currentSpell = spell;
+        }
+
+        public void ValidateSequence()
+        {
+            foreach (var spell in _availableSpells)
+                if (spell._gestureSequence.Count == _gestureManager.ValidatedGestures.Count &&
+                    spell._gestureSequence.SequenceEqual(_gestureManager.ValidatedGestures))
+                {
+                    SetSpell(spell);
+                    Debug.Log("HEEEEEEEEEEEEEEEEEEY" + spell.name);
+                }
+        }
 
         internal void ResetSpell()
         {
             _currentSpell = null;
             _canCast = false;
         }
-
-        public void ValidateSpell()
-        {
-            print(_gestureManager.ValidatedGestures.Count);
-            foreach (var g in _gestureManager.ValidatedGestures) 
-                print(g);
-
-            foreach (var spell in _availableSpells)
-                if (spell._gestureSequence.Count == _gestureManager.ValidatedGestures.Count && 
-                    spell._gestureSequence == _gestureManager.ValidatedGestures)
-                    Debug.Log(spell.name);
-        }
-
     }
 }
