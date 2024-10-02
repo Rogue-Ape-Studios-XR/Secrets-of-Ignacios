6469970548880cf6223d3719a4cd2bb79fcbe9cb
Yori <117444657+Tapardy@users.noreply.github.com>
2024-10-02 11:35:25 +0200

    ✨ Added blowable and resizable

---
+++ Assets/Scripts/Interactables/Earth/Resizable.cs
@@ -0,0 +1,6 @@
+namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Earth
+{
+	internal abstract class Resizable : Interactables
+	{
+	}
+}
---
+++ Assets/Scripts/Interactables/Wind/Blowable.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace RogueApeStudios.SecretsOfIgnacios
+{
+    public class Blowable : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
