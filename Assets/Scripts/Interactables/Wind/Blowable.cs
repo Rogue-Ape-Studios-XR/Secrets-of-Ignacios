9b6e492004be37d90c2f5a43fcb0cbb5f1de30d6
Yori <117444657+Tapardy@users.noreply.github.com>
2024-10-02 11:37:45 +0200

    ♻️ Added proper template to the blowable class

--- Assets/Scripts/Interactables/Wind/Blowable.cs
+++ Assets/Scripts/Interactables/Wind/Blowable.cs
@@ -1,19 +1,6 @@
-using UnityEngine;
-
-namespace RogueApeStudios.SecretsOfIgnacios
+namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 {
-    public class Blowable : MonoBehaviour
-    {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+	internal abstract class Blowable : WindInteractable
+	{
+	}
 }
