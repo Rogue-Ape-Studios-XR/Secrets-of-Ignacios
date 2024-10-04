32a1f13a13a36c6dd22e2b2d7d7012f464197844
Tapardy <117444657+Tapardy@users.noreply.github.com>
2024-10-04 14:27:00 +0200

    ✨ Implemented the proper turnable script.
    Object now turns when hit with the debug spell. You can set a rotation on any axis as its a vecotr3

--- Assets/Scripts/Interactables/Wind/Turnable.cs
+++ Assets/Scripts/Interactables/Wind/Turnable.cs
@@ -1,68 +1,44 @@
-using System;
-using System.Threading;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 {
 	internal class Turnable : WindInteractable
 	{
-		[SerializeField] private bool _isTurning;
 		[SerializeField] private float _turnDuration = 1f;
 		[SerializeField] private Transform _targetTransform;
 		[SerializeField] private Vector3 _rotationDegrees;
-		private CancellationTokenSource _cancellationTokenSource;
+		private float _elapsedTime;
+		private Quaternion _startRotation;
+		private Quaternion _targetRotation;
 
-		internal override void Awake()
+		// No need to override the awake and ondestroy, as theres no Unitask, so they will simply do the base implementation
+		private void FixedUpdate()
 		{
-			base.Awake();
-			_cancellationTokenSource = new CancellationTokenSource();
-		}
-
-		internal override void OnDestroy()
-		{
-			base.OnDestroy();
-			_cancellationTokenSource.Cancel();
-			_cancellationTokenSource.Dispose();
-		}
-
-		private async void Turn(CancellationToken token)
-		{
-			try
-			{
-				var startRotation = _targetTransform.rotation;
-
-				//set a custom degrees in the editor so it will take x amount of seconds to reach this rotation
-				var targetRotation = Quaternion.Euler(_rotationDegrees);
-				var elaspedTime = 0f;
-
-				while (elaspedTime < _turnDuration)
-				{
-					if (token.IsCancellationRequested) throw new OperationCanceledException();
-					elaspedTime += Time.deltaTime;
-					var t = Mathf.Clamp01(elaspedTime / _turnDuration);
-					_targetTransform.rotation = Quaternion.Slerp(startRotation, _targetTransform.rotation, t);
-
-					await UniTask.Yield(token);
-				}
-
-				_targetTransform.rotation = targetRotation;
-				_isTurning = false;
-				_isBlown = false;
-			}
-			catch (OperationCanceledException)
-			{
-				Debug.LogError("turning thingamajing is canceled");
-			}
+			if (_isBlown) Turn();
 		}
 
 		internal override void Blown()
 		{
-			Debug.Log("hit the turning thingamajing");
+			// If there are any issues, you can wrap this in an if (!_isBlown).
+			// This is not done so if the player casts multiple spells, it won't stop with the logic of only 1
+			Debug.Log("hit the turning thingamajig");
 			_isBlown = true;
-			_isTurning = true;
-			Turn(_cancellationTokenSource.Token);
-			Debug.Log("Object is turning");
+
+			_startRotation = _targetTransform.rotation;
+			_targetRotation = _startRotation * Quaternion.Euler(_rotationDegrees);
+			_elapsedTime = 0f;
+
+			Debug.Log("object is turning");
+		}
+
+		private void Turn()
+		{
+			_elapsedTime += Time.deltaTime;
+			var t = Mathf.Clamp01(_elapsedTime / _turnDuration);
+
+			_targetTransform.rotation = Quaternion.Slerp(_startRotation, _targetRotation, t);
+
+			if (t >= 1f) _isBlown = false;
 		}
 	}
 }
--- Assets/Scripts/Interactables/Wind/WindInteractable.cs
+++ Assets/Scripts/Interactables/Wind/WindInteractable.cs
@@ -6,8 +6,9 @@ namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 	internal abstract class WindInteractable : Interactables
 	{
 		[SerializeField] internal bool _isBlown;
-		internal event Action<bool> onBlown;
 
+		internal event Action<bool> onBlown;
+		
 		internal override void Awake()
 		{
 			_spellReceiver.OnSpellReceived += HandleSpellReceived;
@@ -29,6 +30,8 @@ namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 					break;
 				case "Wind":
 					// Implement the spinning of the fan. Boxes and things are not required, as the wind spell will have force
+					Blown();
+					onBlown?.Invoke(_isBlown);
 					break;
 				default: throw new NotImplementedException();
 			}
