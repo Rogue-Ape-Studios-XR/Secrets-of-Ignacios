9c57747cb1c72e2e63718971f16d844404114d35
Yori <117444657+Tapardy@users.noreply.github.com>
2024-10-04 11:14:27 +0200

    ✨ Added some override variables to wininteractable.
    Mainly a push because laptop doesn't want to work with me

--- Assets/Scripts/Interactables/Wind/WindInteractable.cs
+++ Assets/Scripts/Interactables/Wind/WindInteractable.cs
@@ -1,7 +1,12 @@
+using System;
+using UnityEngine;
+
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 {
 	internal abstract class WindInteractable : Interactables
 	{
+		[SerializeField] internal bool isBlown;
+
 		internal override void Awake()
 		{
 			_spellReceiver.OnSpellReceived -= HandleSpellReceived;
@@ -11,9 +16,24 @@ namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 		{
 			_spellReceiver.OnSpellReceived -= HandleSpellReceived;
 		}
+		
+		internal event Action<bool> onBlown; 
 
 		internal override void HandleSpellReceived(string spellType)
 		{
+			Debug.Log("Some spell hit");
+			switch (spellType)
+			{
+				case "Debug":
+					// Logic for casting a fire spell
+					break;
+				case "Wind":
+					// Implement the spinning of the fan. Boxes and things are not required, as the wind spell will have force
+					break;
+				default: throw new NotImplementedException();
+			}
 		}
+		
+		internal abstract void HandleBlown();
 	}
 }
