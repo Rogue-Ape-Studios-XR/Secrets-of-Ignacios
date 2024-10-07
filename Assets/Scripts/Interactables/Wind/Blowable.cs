a2d0bdbeba8787185f2fbc5b892182fda3722370
Tapardy <117444657+Tapardy@users.noreply.github.com>
2024-10-07 11:57:48 +0200

    ✨ Added the functionality of the Blowable script

--- Assets/Scripts/Interactables/Wind/Blowable.cs
+++ Assets/Scripts/Interactables/Wind/Blowable.cs
@@ -1,9 +1,40 @@
+using System;
+using UnityEngine;
+
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 {
 	internal class Blowable : WindInteractable
 	{
+		// Duration something stays in the blown state
+		[SerializeField] private float _blownDuration = 1f;
+
+		// Bool to decide if something stays blown or not.
+		[SerializeField] private bool _temporaryBlown;
+
+		private float _elapsedTime;
+		// Event to indicate an object is hit with the wind spell, thus blown :)
+		internal event Action<bool> onObjectBlown; 
+
+		private void FixedUpdate()
+		{
+			if (_isBlown && _temporaryBlown)
+			{
+				_elapsedTime += Time.fixedDeltaTime;
+				if (_elapsedTime >= _blownDuration) EndBlown();
+			}
+		}
+
 		internal override void Blown()
 		{
+			_isBlown = true;
+			onObjectBlown?.Invoke(_isBlown);
+			_elapsedTime = 0f;
+		}
+
+		private void EndBlown()
+		{
+			_isBlown = false;
+			onObjectBlown?.Invoke(_isBlown);
 		}
 	}
 }
