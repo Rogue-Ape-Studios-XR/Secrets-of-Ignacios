7a60b618c774ffd8693909533344499f9deeb461
Tapardy <117444657+Tapardy@users.noreply.github.com>
2024-10-14 16:57:27 +0200

    🚧 Added a first version of a resize
    Currently only in editor, and this push is mostly so I can access it on my laptop tbh

--- Assets/Scripts/Interactables/Earth/EarthInteractable.cs
+++ Assets/Scripts/Interactables/Earth/EarthInteractable.cs
@@ -1,10 +1,16 @@
+using System;
+using UnityEngine;
+
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Earth
 {
 	internal abstract class EarthInteractable : Interactables
 	{
+		[SerializeField] internal bool _isTouched;
+		internal event Action<bool> onTouched;
+		
 		internal override void Awake()
 		{
-			_spellReceiver.OnSpellReceived -= HandleSpellReceived;
+			_spellReceiver.OnSpellReceived += HandleSpellReceived;
 		}
 
 		internal override void OnDestroy()
@@ -14,6 +20,21 @@ namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Earth
 
 		internal override void HandleSpellReceived(string spellType)
 		{
+			Debug.Log("Earth spell hit");
+
+			switch (spellType)
+			{
+				case "Debug":
+					Touched();
+					onTouched?.Invoke(_isTouched);
+					break;	
+				case "Earth":
+					Touched();
+					onTouched?.Invoke(_isTouched);
+					break;
+			}
 		}
+
+		internal abstract void Touched();
 	}
 }
--- Assets/Scripts/Interactables/Earth/Resizable.cs
+++ Assets/Scripts/Interactables/Earth/Resizable.cs
@@ -1,6 +1,66 @@
+using System;
+using UnityEngine;
+
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Earth
 {
-	internal abstract class Resizable : Interactables
-	{
-	}
+    internal class Resizable : EarthInteractable
+    {
+        [SerializeField] private Vector3 _initialScale;
+        [SerializeField] private Transform _targetObject;
+        [SerializeField] private Vector3[] _premadeSizes;
+        [SerializeField] private bool _usesPremadeSizes;
+        [SerializeField, Range(0f, 2f)] private float _resizeValue;
+
+        internal override void Awake()
+        {
+            base.Awake();
+            _initialScale = _targetObject.localScale;
+           
+        }
+
+        internal override void Touched()
+        {
+            _isTouched = true;
+            _targetObject.localScale = _initialScale;
+        }
+
+        private void OnValidate()
+        {
+            ResizeObjectInEditor();
+        }
+
+        //Just for testing atm, still trying to figure out how the hell I do this
+        private void ResizeObjectInEditor()
+        {
+            if (_targetObject == null) return;
+
+            Vector3 newScale = _initialScale * _resizeValue;
+
+            if (_usesPremadeSizes && _premadeSizes.Length > 0)
+            {
+                newScale = FindClosestPremadeSize(newScale);
+            }
+
+            _targetObject.localScale = newScale;
+        }
+
+        //Doesn't work properly yet
+        private Vector3 FindClosestPremadeSize(Vector3 targetScale)
+        {
+            Vector3 closestSize = _premadeSizes[0];
+            float closestDistance = Vector3.Distance(targetScale, closestSize);
+
+            foreach (Vector3 size in _premadeSizes)
+            {
+                float distance = Vector3.Distance(targetScale, size);
+                if (distance < closestDistance)
+                {
+                    closestSize = size;
+                    closestDistance = distance;
+                }
+            }
+
+            return closestSize;
+        }
+    }
 }
