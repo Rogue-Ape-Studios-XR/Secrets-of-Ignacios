92fc3e325776e6d768cafc2faab8f3f8afbbc34f
Luc Vogelsangs <lucvogelsangs@gmail.com>
2024-10-16 11:05:16 +0200

    ⚡️Script rework and bracelet button improvement

--- Assets/Scripts/UI/UIButtonTrigger.cs
+++ Assets/Scripts/UI/UIButtonTrigger.cs
@@ -6,13 +6,23 @@ using UnityEngine.UI;
 namespace RogueApeStudios.SecretsOfIgnacios
 {
     [RequireComponent(typeof(BoxCollider))]
-    public class UIButtonTrigger : Button
+    public class UIButtonTrigger : MonoBehaviour
     {
+        [SerializeField] private GrimoireUIController _bookcontroller;
+        [SerializeField] private int _selectedChapter;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("FingerTip"))
             {
-                gameObject.GetComponent<Button>().onClick.Invoke();
+                if (gameObject.CompareTag("UIButtonTab"))
+                {
+                    _bookcontroller.ActivateChapter(_selectedChapter);
+                }
+                else if (gameObject.CompareTag("UIButtonBracelet"))
+                {
+                    _bookcontroller.Summongrimoire();
+                }
             }
         }
     }
