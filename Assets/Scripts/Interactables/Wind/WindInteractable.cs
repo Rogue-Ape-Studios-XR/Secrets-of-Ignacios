6c9b84dc1e005085de9d2a4073d1d05cd4d75189
Timsel1 <timmeijvogel2003@gmail.com>
2024-10-17 16:24:51 +0200

    ♻️ Update onBlown to OnBlown

--- Assets/Scripts/Interactables/Wind/WindInteractable.cs
+++ Assets/Scripts/Interactables/Wind/WindInteractable.cs
@@ -3,40 +3,40 @@ using UnityEngine;
 
 namespace RogueApeStudios.SecretsOfIgnacios.Interactables.Wind
 {
-	internal abstract class WindInteractable : Interactables
-	{
-		[SerializeField] internal bool _isBlown;
+    internal abstract class WindInteractable : Interactables
+    {
+        [SerializeField] internal bool _isBlown;
 
-		internal event Action<bool> onBlown;
-		
-		internal override void Awake()
-		{
-			_spellReceiver.OnSpellReceived += HandleSpellReceived;
-		}
+        internal event Action<bool> OnBlown;
 
-		internal override void OnDestroy()
-		{
-			_spellReceiver.OnSpellReceived -= HandleSpellReceived;
-		}
+        internal override void Awake()
+        {
+            _spellReceiver.OnSpellReceived += HandleSpellReceived;
+        }
 
-		internal override void HandleSpellReceived(string spellType)
-		{
-			Debug.Log("Some wind spell hit");
-			switch (spellType)
-			{
-				case "Debug":
-					Blown();
-					onBlown?.Invoke(_isBlown);
-					break;
-				case "Wind":
-					// Implement the spinning of the fan. Boxes and things are not required, as the wind spell will have force
-					Blown();
-					onBlown?.Invoke(_isBlown);
-					break;
-				default: throw new NotImplementedException();
-			}
-		}
+        internal override void OnDestroy()
+        {
+            _spellReceiver.OnSpellReceived -= HandleSpellReceived;
+        }
 
-		internal abstract void Blown();
-	}
+        internal override void HandleSpellReceived(string spellType)
+        {
+            Debug.Log("Some wind spell hit");
+            switch (spellType)
+            {
+                case "Debug":
+                    Blown();
+                    OnBlown?.Invoke(_isBlown);
+                    break;
+                case "Wind":
+                    // Implement the spinning of the fan. Boxes and things are not required, as the wind spell will have force
+                    Blown();
+                    OnBlown?.Invoke(_isBlown);
+                    break;
+                default: throw new NotImplementedException();
+            }
+        }
+
+        internal abstract void Blown();
+    }
 }
