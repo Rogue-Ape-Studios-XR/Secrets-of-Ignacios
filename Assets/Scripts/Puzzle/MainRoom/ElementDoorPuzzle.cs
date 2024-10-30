78745f0d526310a59e617319e518a30a52fafe0f
LJRReyniers <93527848+LJRReyniers@users.noreply.github.com>
2024-10-30 13:06:15 +0100

    ✨ Added puzzle prototypes for the doors to the wind and earth rooms

---
+++ Assets/Scripts/Puzzle/MainRoom/ElementDoorPuzzle.cs
@@ -0,0 +1,42 @@
+using RogueApeStudios.SecretsOfIgnacios.Interactables.Fire;
+using RogueApeStudios.SecretsOfIgnacios.Interactables.Water;
+using RogueApeStudios.SecretsOfIgnacios.Interactables.Wind;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RogueApeStudios.SecretsOfIgnacios
+{
+    public class ElementDoorPuzzle : MonoBehaviour
+    {
+        [SerializeField] private List<Interactables.Interactables> _targets;
+
+        [SerializeField] private Fillable _waterTarget;
+        [SerializeField] private Blowable _windTarget;
+        [SerializeField] private PersistentFire _fireTarget;
+
+        [SerializeField] private Animator _animator;
+
+        private void Awake()
+        {
+            _waterTarget.onFilled += TargetCheck;
+            _windTarget.onBlown += TargetCheck;
+            _fireTarget.OnIgnitionToggle += TargetCheck;
+        }
+
+        private void OnDestroy()
+        {
+            _waterTarget.onFilled -= TargetCheck;
+            _windTarget.onBlown -= TargetCheck;
+            _fireTarget.OnIgnitionToggle -= TargetCheck;
+        }
+
+        private void TargetCheck(bool hit)
+        {
+            if (_waterTarget._filled && _windTarget._isBlown && _fireTarget._isOnFire)
+            {
+                Debug.Log("Door opens");
+                _animator.SetTrigger("DoorOpen");
+            }
+        }
+    }
+}
