b12ff164d524301636f285f837a1523493c867ab
Tapardy <117444657+Tapardy@users.noreply.github.com>
2024-12-09 15:27:21 +0100

    ✨ Added areaprogressionmanager which you add to the prefab

---
+++ Assets/Scripts/Progression/AreaProgression/AreaProgressionManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace RogueApeStudios.SecretsOfIgnacios.Progression.AreaProgression
+{
+    public class AreaProgressionManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            ProgressionManager.OnProgressionEvent += HandleProgressionEvent;
+        }
+
+        private void OnDisable()
+        {
+            ProgressionManager.OnProgressionEvent -= HandleProgressionEvent;
+        }
+
+        private void HandleProgressionEvent(ProgressionData data)
+        {
+            if (data.Type == ProgressionType.AreaUnlock && data.Data is AreaUnlockData areaData)
+            {
+                if (areaData.Area != null)
+                {
+                    Debug.Log($"area {areaData.Area.name} has been unlocked");
+                    areaData.Area.SetActive(true);
+                }
+            }
+        }
+    }
+}
