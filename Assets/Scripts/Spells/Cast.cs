906caeee2fd9b6dcb4c7decc6cb02a35939c0440
Davopia <davey2206@live.nl>
2024-12-17 11:42:35 +0100

    🚑️Moved water sound effects to hand data

--- Assets/Scripts/Spells/Cast.cs
+++ Assets/Scripts/Spells/Cast.cs
@@ -18,7 +18,6 @@ namespace RogueApeStudios.SecretsOfIgnacios.Spells
         [SerializeField] internal HandData _leftHandData;
         [SerializeField] private HandVfxManager _handVfxManager;
         [SerializeField] private AudioSource _audioSource;
-        [SerializeField] private AudioSource _audioSourceWater;
 
         [Header("Casting Mode")]
         [SerializeField] private float _castTimer = 0.75f;
@@ -97,7 +96,7 @@ namespace RogueApeStudios.SecretsOfIgnacios.Spells
 
         private async void RepeatedCast(HandData handData)
         {
-            _audioSourceWater.Play();
+            handData.PlaySound();
             handData._isCasting = true;
             while (handData._canCast)
             {
@@ -123,7 +122,7 @@ namespace RogueApeStudios.SecretsOfIgnacios.Spells
         {
             if (_spellManager.CurrentSpell is not null && handData._isCasting)
             {
-                _audioSourceWater.Stop();
+                handData.StopSound();
                 handData._isCasting = false;
                 handData._canCast = false;
                 handData._renderer.materials[1].SetColor("_MainColor", handData._defaultColor);
