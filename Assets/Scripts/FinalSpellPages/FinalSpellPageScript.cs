8210de0a13884e73460f65317086d9a7d7469cc6
Tapardy <117444657+Tapardy@users.noreply.github.com>
2025-01-20 13:44:05 +0100

    ✨ Added code so pages can now increment a counter to unlock final spell itself

---
+++ Assets/Scripts/FinalSpellPages/FinalSpellPageScript.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace RogueApeStudios.SecretsOfIgnacios.FinalSpellPages
+{
+    public class FinalSpellPageScript : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private VisualEffect _poofEffect;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public static event Action onPagePickup;
+        private void Awake()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private void OnDestroy()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
+        
+        // Yeah this is really ugly and stupid but I am not changing the grab code and risking a conflict in the last week
+        private void FixedUpdate()
+        {
+            // 14 is the layer the grab logic sets grabbed objects to
+            // so by abusing this we can circumvent an event. The update only runs on 5 pages anyways so who cares
+            if (gameObject.layer == 14)
+            {
+                _poofEffect.Play();
+                onPagePickup?.Invoke();
+                // first just disable mesh renderer so its gone to the player, but won't mess with the vfx
+                _meshRenderer.enabled = false;
+                HandlePagePickupAsync(_cancellationTokenSource.Token).Forget();   
+            }
+        }
+        private async UniTaskVoid HandlePagePickupAsync(CancellationToken token)
+        {
+            try
+            {
+                while (_poofEffect.aliveParticleCount > 0)
+                {
+                    await UniTask.Yield(PlayerLoopTiming.Update, token);
+                }
+
+                Destroy(gameObject);
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.LogError("HandlePagePickupAsync was canceled...");
+            }
+        }
+    }
+}
--- Assets/Scripts/Spells/FinalSpellTrigger.cs
+++ Assets/Scripts/Spells/FinalSpellTrigger.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using RogueApeStudios.SecretsOfIgnacios.FinalSpellPages;
+using RogueApeStudios.SecretsOfIgnacios.Progression;
 using UnityEngine;
 
 namespace RogueApeStudios.SecretsOfIgnacios.Spells
@@ -11,24 +13,68 @@ namespace RogueApeStudios.SecretsOfIgnacios.Spells
         public static event Action<bool> onFinalSpellUnlockStateChange;
 
         [SerializeField] private List<string> _tags;
+        [SerializeField] private List<Spell> _finalSpells;
+        // serializing the bool so you can just enable it for testing
+        [SerializeField] private bool _allPagesCollected;
+
+        private void Start()
+        {
+            FinalSpellPageCounter.onAllPagesCollected += HandleAllPagesCollected;
+        }
+
+        private void OnDestroy()
+        {
+            FinalSpellPageCounter.onAllPagesCollected -= HandleAllPagesCollected;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log(other.name);
-            if (_tags.Contains(other.tag))
+            if (_tags.Contains(other.tag) && _allPagesCollected)
             {
                 onFinalSpellUnlockStateChange?.Invoke(true);
+                foreach (var finalSpell in _finalSpells)
+                {
+                    if (finalSpell != null)
+                    {
+                        ProgressionData progressionData = new ProgressionData
+                        {
+                            Type = ProgressionType.SpellUnlock,
+                            Data = new SpellUnlockData() { Spell = finalSpell }
+                        };
+
+                        ProgressionManager.TriggerProgressionEvent(progressionData);
+                    }
+                }
                 Debug.Log("Unlocked the final spell");
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if (_tags.Contains(other.tag))
+            if (_tags.Contains(other.tag) && _allPagesCollected)
             {
                 onFinalSpellUnlockStateChange?.Invoke(false);
+                foreach (var finalSpell in _finalSpells)
+                {
+                    if (finalSpell != null)
+                    {
+                        ProgressionData progressionData = new ProgressionData
+                        {
+                            Type = ProgressionType.SpellLock,
+                            Data = new SpellLockData() { Spell = finalSpell }
+                        };
+
+                        ProgressionManager.TriggerProgressionEvent(progressionData);
+                    }
+                }
                 Debug.Log("Player exited the area, locking the spell");
             }
+        }
+
+        private void HandleAllPagesCollected()
+        {
+            _allPagesCollected = true;
         }
     }
 }
